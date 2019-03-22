8cabac7ac1ceb91f03e708a94ea9f1c6dbadbc01
stacyy83 <stacyy83@gmail.com>
2019-03-23 01:53:57 +0800

    recipe頁面swiper

---
+++ js/site/recipe.js
@@ -0,0 +1,12 @@
+$(function(){
+    var swiper_recipe = new Swiper('.swiper-container--recipe', {
+        loop: true,
+        speed: 1000,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+        },
+        autoplay: true,
+    });
+
+});
