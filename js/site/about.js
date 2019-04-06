4d9a7eb0550f35e21a9f26fc78484d02a7c5f070
stacyy83 <stacyy83@gmail.com>
2019-04-07 01:04:47 +0800

    crop頁面切版進行中

---
+++ js/site/about.js
@@ -0,0 +1,13 @@
+$(function(){
+    $('#scroll_down').click(function(){
+        $('html,body').animate({scrollTop:$('#about_2').offset().top}, 500);
+    });
+    $('#scroll_top').click(function(){
+        $('html,body').animate({scrollTop:$('#about_1').offset().top}, 500);
+    });
+    $(document).on("scroll", function(e){ 
+        console.log(e);
+            
+    });
+
+});
