98c2a77df34efa6dd027647ab776a5f65b70c991
stacyy83 <stacyy83@gmail.com>
2019-04-10 03:18:22 +0800

    島嶼動畫隨滑鼠左右移動

--- js/site/island.js
+++ js/site/island.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
     var x = 0;
     var y = -90;
     var bgColor = ['#006781', '#4B8D32', '#D6CD25', '#006857', '#ED7235', '#FF947B', '#AF485C', '#F7B42F', '#AE7516', '#C99A6B', '#2E8E89', '#325984']
@@ -8,7 +8,7 @@ $(function(){
 
     $('._island_intro__seasonSection--1').addClass('active');
     $('._island_intro__row--x').addClass('active');
-    
+
     //island environment background slider  
     var swiper_bg = new Swiper('.swiper-container--bg', {
         loop: true,
@@ -17,79 +17,79 @@ $(function(){
             el: '.swiper-pagination',
             dynamicBullets: true,
         },
-        mousewheel:true
+        mousewheel: true
     });
 
     // 讓island移到下一個
-    swiper_bg.on('slideNextTransitionStart', function(){
+    swiper_bg.on('slideNextTransitionStart', function () {
         console.log('next');
-        x = x+90;
-        y = y+90;
-        $('._island_intro__row--x').css("transform","rotate("+x+ "deg)");
-        $('._island_intro__row--y').css("transform","rotate("+y+ "deg)");
+        x = x + 90;
+        y = y + 90;
+        $('._island_intro__row--x').css("transform", "rotate(" + x + "deg)");
+        $('._island_intro__row--y').css("transform", "rotate(" + y + "deg)");
     });
     // 讓island移回上一個
-    swiper_bg.on('slidePrevTransitionStart', function(){
+    swiper_bg.on('slidePrevTransitionStart', function () {
         console.log('prev');
-        x = x-90;
-        y = y-90;
-        $('._island_intro__row--x').css("transform","rotate("+x+ "deg)");
-        $('._island_intro__row--y').css("transform","rotate("+y+ "deg)");
+        x = x - 90;
+        y = y - 90;
+        $('._island_intro__row--x').css("transform", "rotate(" + x + "deg)");
+        $('._island_intro__row--y').css("transform", "rotate(" + y + "deg)");
     });
 
 
-    swiper_bg.on('slideChangeTransitionStart', function(){
+    swiper_bg.on('slideChangeTransitionStart', function () {
         console.log(this.activeIndex);
         index = this.activeIndex;
 
-        if(index == 13){
+        if (index == 13) {
             index = 1;
-        }else if(index == 0){
+        } else if (index == 0) {
             index = 12;
         }
         // circle背景色切換
         var i = index - 1;
-        $('._island_intro__circle').css("background-color",bgColor[i]);
-        $('._island_intro__view').css("background-color",bgColor[i]);
+        $('._island_intro__circle').css("background-color", bgColor[i]);
+        $('._island_intro__view').css("background-color", bgColor[i]);
         animate(i);
         // 1-4月顯示
-        if(index == 1 || index == 4){
+        if (index == 1 || index == 4) {
             console.log('circle1');
             $('._island_intro__seasonSection').removeClass('active');
             $('._island_intro__seasonSection--1').addClass('active');
         }
         // 5-8月顯示
-        if(index == 5 || index == 8){
+        if (index == 5 || index == 8) {
             console.log('circle2');
             $('._island_intro__seasonSection').removeClass('active');
             $('._island_intro__seasonSection--2').addClass('active');
         }
 
         // 9-12月顯示
-        if(index == 9 || index == 12){
+        if (index == 9 || index == 12) {
             console.log('circle3');
             $('._island_intro__seasonSection').removeClass('active');
             $('._island_intro__seasonSection--3').addClass('active');
         }
 
-        
-        if(index%2 == 1 ){
+
+        if (index % 2 == 1) {
             //x軸月份顯示
             $('._island_intro__row--x').addClass('active');
             $('._island_intro__row--y').removeClass('active');
-        }else{
+        } else {
             //y軸月份顯示
             $('._island_intro__row--y').addClass('active');
             $('._island_intro__row--x').removeClass('active');
         }
     });
 
-    function animate(i){
+    function animate(i) {
         animIndex = i;
     }
 
     //點了島嶼瀏覽的view
-    $('._island_intro__btn').click(function(){
+    $('._island_intro__btn').click(function () {
         var anim;
         console.log(animIndex);
         var animData = {
@@ -97,46 +97,98 @@ $(function(){
             renderer: 'svg',
             loop: true,
             autoplay: true,
-            path: '/2019inseason/img/json/'+animArray[0]
+            path: '/2019inseason/img/json/' + animArray[0]
         };
 
         // 先執行bodymovin才不會卡卡
         anim = bodymovin.loadAnimation(animData);
-        
+
         //等bodymovin生出svg時再執行parallax.js
-        anim.addEventListener('DOMLoaded', function(){
+        anim.addEventListener('DOMLoaded', function () {
 
-                // $("#bodymovin svg").attr("data-depth",1);
-                // var scene = document.getElementById('bodymovin');
-                // var parallaxInstance = new Parallax(scene, {
-                //     relativeInput: true,
-                //     // hoverOnly: true,
-                //     originX:0,
-                //     limitY:0,
-                //     limitX: $("#bodymovin svg").width()-$(window).width(),
-                    
-                // });  
+            // $("#bodymovin svg").attr("data-depth",1);
+            // var scene = document.getElementById('bodymovin');
+            // var parallaxInstance = new Parallax(scene, {
+            //     relativeInput: true,
+            //     // hoverOnly: true,
+            //     originX:0,
+            //     limitY:0,
+            //     limitX: $("#bodymovin svg").width()-$(window).width(),
 
-                // 產物點選
-                $('#cabbage').click(function(){
-                    $('#js_cabbage').addClass('active');
-                    $('html').addClass('popup');
-                    console.log('click');
-                });
+            // });  
+
+            var container = document.querySelector("#bodymovin");
+            var mover = document.querySelector("#bodymovin svg");
+            var moveX = 0;
+            var container_wd = $('#anim-container').innerWidth();
+            var mover_wd = $('#bodymovin svg').innerWidth();
+            var result = 0;
+            container.addEventListener("mousemove", function (e) {
+                // console.log(e.pageX + ", " + e.pageY);
+                // console.log(e.offsetX + ", " + e.offsetY);
+                // console.log(-(e.pageX / container_wd) * (mover_wd - container_wd));
+                result = (e.pageX / container_wd) * (container_wd - mover_wd);
+                mover.style.transform = "translate3d(" + Math.round(result) + 'px'+",0px , 0px)";
+                // console.log(Math.round(result));
+               
+
+
+                
+                // if(e.pageX>0 && e.pageX<200){
+                //     moveX = moveX+10;
+                //     if(moveX>0){
+                //         moveX = 0;
+                //         // move = 0;
+                //     }else if(moveX < 0){
+                //         mover.style.transform = "translate3d("+ moveX + 'px'+",0px , 0px)";
+                //     }
+                //     console.log(moveX);
+                // }else if(e.pageX>1700 && e.pageX<1919){
+                //     moveX = moveX - 10;
+                //     if(moveX< -875){
+                //         moveX = -875;
+                //         // move = -875;
+                //     }else if(moveX > -875){
+                //         mover.style.transform = "translate3d("+ moveX + 'px'+",0px , 0px)";
+                //     }
+                //     console.log(moveX);
+                // }
+            });
+        
+            // container.addEventListener("mouseenter", function () {
+        
+            //     setTimeout(function () {
+            //         mover.classList.add("no-more-slidey");
+            //         container.removeEventListener("mouseenter");
+            //     }, 250);
+        
+            // });
+
+
+
+
+            // 產物點選
+            $('#cabbage').click(function () {
+                $('#js_cabbage').addClass('active');
+                $('html').addClass('popup');
+                console.log('click');
+            });
         });
-        setTimeout(function(){
+        setTimeout(function () {
             $('._island_intro').addClass('active');
             $('._island_intro__bgImg').addClass('active');
             $('._island_detail__bg').addClass('active');
         }, 1000);
-        setTimeout(function(){
-            $('._island_intro').css("display","none");
-        },2000);
+        setTimeout(function () {
+            $('._island_intro').css("display", "none");
+        }, 2000);
     });
 
-    
-    $('.js-crop_close').click(function(){
+
+    $('.js-crop_close').click(function () {
         $('html').removeClass('popup');
         $(this).parent('._island_crop').removeClass('active');
     });
+
+    
 });
