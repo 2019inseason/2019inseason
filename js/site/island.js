7e70f59274e28d3474121340952136c05cc1cdf3
stacyy83 <stacyy83@gmail.com>
2019-04-18 03:45:36 +0800

    hover finish

--- js/site/island.js
+++ js/site/island.js
@@ -3,7 +3,7 @@ $(function () {
     var y = -90;
     var bgColor = ['#006781', '#4B8D32', '#D6CD25', '#006857', '#ED7235', '#FF947B', '#AF485C', '#F7B42F', '#AE7516', '#C99A6B', '#2E8E89', '#325984']
     var index = 0;
-    var animArray = ['jan.json','feb.json','mar.json','apr.json','may.json','jun.json','july.json','aug.json','sep.json','oct.json'];
+    var animArray = ['jan.json','feb.json','mar.json','apr.json','may.json','jun.json','july.json','aug.json','sep.json','oct.json','nov.json','dec.json'];
     var animIndex = 0;
     var hoverIndex = 1;
 
@@ -130,7 +130,7 @@ $(function () {
             var mouseX=0
             container.addEventListener("mousemove", function (e) {
                 
-                // mouseX = (e.pageX - (container_wd / 2)) / (container_wd - mover_wd)/ 5;
+                mouseX = (e.pageX - (container_wd / 2)) / (container_wd - mover_wd)/ 5;
                 
 
                 // result = (e.pageX / container_wd) * (container_wd - mover_wd);
@@ -164,28 +164,18 @@ $(function () {
             });
 
             //滑鼠左右移動
-            // setInterval(function(){
-            //     var speed= mouseX * 0.01;
+            setInterval(function(){
+                var speed= mouseX * 0.01;
 
-            //     result += ((container_wd-mover_wd)-result)*speed;
-            //     if(result<0){
-            //         result = 0;
-            //     }else if(result>mover_wd-container_wd){
-            //         result = mover_wd-container_wd;
-            //     }
-            //     mover.style.transform = "translate3d(" + -Math.round(result) + 'px'+",0px , 0px)";
-            // },10);
+                result += ((container_wd-mover_wd)-result)*speed;
+                if(result<0){
+                    result = 0;
+                }else if(result>mover_wd-container_wd){
+                    result = mover_wd-container_wd;
+                }
+                mover.style.transform = "translate3d(" + -Math.round(result) + 'px'+",0px , 0px)";
+            },10);
 
-            // function makeSVG(tag, attrs) {
-            //     var el= document.createElementNS('http://www.w3.org/2000/svg', tag);
-            //     for (var k in attrs)
-            //         el.setAttribute(k, attrs[k]);
-            //     return el;
-            // }
-            // var test1= makeSVG('svg', {id: 'test1', xmlns:'http://www.w3.org/2000/svg',x:-2.4, y:-2.4});
-            // document.getElementById('cabbage').appendChild(test1);
-            // var test2= makeSVG('path', {id: 'test2', d:'M138.44,14.59c-1-.76-2.07-1.62-3.21-2.66a33.66,33.66,0,0,1-4.87-5.57C126.65,1.1,122.26,0,119.23,0a15,15,0,0,0-7.32,2C97,5.1,94.8,15.72,94.55,20.25c-.39,1-.75,2-1.08,3.16a12.39,12.39,0,0,0-3.39-.47,14.85,14.85,0,0,0-6.18,1.44,17.4,17.4,0,0,0-5.08-5.31c-1-.76-2-1.61-3.18-2.65a33.66,33.66,0,0,1-4.87-5.57c-3.71-5.26-8.1-6.36-11.13-6.36a15,15,0,0,0-7.32,2C37.42,9.59,35.21,20.21,35,24.74a48.52,48.52,0,0,0-2.35,8.6l0,.12a12.18,12.18,0,0,0-5.2-1.14,15.09,15.09,0,0,0-7.31,2C5.18,37.42,3,48,2.72,52.56A47.81,47.81,0,0,0,.37,61.18l0,.18C-.38,65-1.44,77.14,14.63,81.71c1.06.3,2.12.57,3.15.81a42.43,42.43,0,0,0,9.5,1.22,20.23,20.23,0,0,0,9.09-1.88C47.3,77.65,53.09,69.67,53.09,58.78a17.09,17.09,0,0,0-.33-3.51,39.13,39.13,0,0,0,6.75.65,25,25,0,0,0,3.18-.19c.07,5,2.23,13.08,14.62,16.61,1.07.3,2.13.57,3.15.81A42.5,42.5,0,0,0,90,74.37a20.23,20.23,0,0,0,9.09-1.88c10.28-4,16-11.29,16.66-21.24a33.74,33.74,0,0,0,3.4.18,20.15,20.15,0,0,0,9.08-1.88c10.94-4.21,16.72-12.19,16.72-23.08C144.91,19.68,141.77,17.21,138.44,14.59Zm-11,32.66A17.56,17.56,0,0,1,119.14,49a35.52,35.52,0,0,1-5.82-.53c0,.29,0,.6,0,.92,0,9.92-5.08,16.91-15.09,20.78A17.59,17.59,0,0,1,90,72a44.59,44.59,0,0,1-12-2c-5.53-1.57-9.34-4.29-11.34-8.1a14.45,14.45,0,0,1-1.28-9.2,19.61,19.61,0,0,1-5.81.81,43.38,43.38,0,0,1-11.08-1.7c1.38,1.61,2.19,3.58,2.19,7,0,9.92-5.08,16.91-15.09,20.78a17.59,17.59,0,0,1-8.25,1.77,44.6,44.6,0,0,1-12-2C9.77,77.8,6,75.08,4,71.27a14.51,14.51,0,0,1-1.24-9.44l0-.23a44.54,44.54,0,0,1,2.37-8.51,17.38,17.38,0,0,1,1.12-5.66C9,40.34,15.75,37.65,21,36.64c.46-.3,7.57-4.76,13.71,1.23A15.76,15.76,0,0,1,35,34l0-.23a44.54,44.54,0,0,1,2.37-8.51,17.6,17.6,0,0,1,1.11-5.66C41.21,12.52,48,9.83,53.22,8.82c.5-.33,9.11-5.73,15.57,3.43a36.15,36.15,0,0,0,5.21,6h0c1.21,1.1,2.34,2,3.35,2.78,2.53,2,4.41,3.48,5.17,6.52l1.14-.24c.4-.27,6-3.74,11.45-.51a38.48,38.48,0,0,1,1.85-6,17.6,17.6,0,0,1,1.11-5.66c2.72-7.09,9.51-9.79,14.74-10.79.5-.33,9.11-5.73,15.57,3.43a36.15,36.15,0,0,0,5.21,6c1.2,1.1,2.34,2,3.35,2.78,3.34,2.63,5.55,4.36,5.55,10C142.49,36.39,137.4,43.38,127.39,47.25Z', fill: '#fff',opacity:'0.8'});
-            // document.getElementById('test1').appendChild(test2);
 
             // 做hover的圖
             function makeImg(h,x,y,item){
@@ -193,7 +183,7 @@ $(function () {
                 img.setAttributeNS(null,'height',h);
                 img.setAttributeNS(null,'x',x);
                 img.setAttributeNS(null,'y',y);
-                // img.setAttributeNS(null,'opacity','0.8');
+                img.setAttributeNS(null,'opacity','0.8');
                 img.setAttributeNS('http://www.w3.org/1999/xlink','href','img/island-hover/'+ hoverIndex +'/'+ item +'.svg');
                 return img;
             }
@@ -249,6 +239,116 @@ $(function () {
                     $('#watermelon').append(makeImg(84,-2.8,-2,'watermelon'));
                     $('#tauzi').append(makeImg(104,-2,-1.5,'tauzi'));
                 break
+                case 5:
+                    $('#pumpkin').append(makeImg(64.5,-2.4,-1.2,'pumpkin'));
+                    $('#lusion').append(makeImg(62,3.7,-1.8,'lusion'));
+                    $('#melon').append(makeImg(60,-2,-1.8,'melon'));
+                    $('#kongshintsai').append(makeImg(83,-1.2,-1.8,'kongshintsai'));
+                    $('#longshutsai').append(makeImg(71,-1.6,-1.6,'longshutsai'));
+                    $('#kukua').append(makeImg(117,-1.3,-1.5,'kukua'));
+                    $('#watermelon').append(makeImg(86.2,-2.2,-2,'watermelon'));
+                    $('#tauzi').append(makeImg(137,-1.6,-1.6,'tauzi'));
+                    $('#pear').append(makeImg(104,-1.2,-1,'pear'));
+                    $('#liangwu').append(makeImg(110,-2,-1.8,'liangwu'));
+                    $('#lizi').append(makeImg(118,-1.8,-2,'lizi'));
+                break
+                case 6:
+                    $('#pumpkin').append(makeImg(115,-1.5,-1.6,'pumpkin'));
+                    $('#lusion').append(makeImg(66,2.8,-1.8,'lusion'));
+                    $('#melon').append(makeImg(89.5,-1.8,-1.6,'melon'));
+                    $('#kongshintsai').append(makeImg(65,-1.4,-1.8,'kongshintsai'));
+                    $('#longshutsai').append(makeImg(71,-1.6,-1.6,'longshutsai'));
+                    $('#kukua').append(makeImg(128,-1.4,-1.4,'kukua'));
+                    $('#watermelon').append(makeImg(98.2,-2,-1.6,'watermelon'));
+                    $('#tauzi').append(makeImg(139,-2,-1.8,'tauzi'));
+                    $('#pear').append(makeImg(104,-1.2,-1.2,'pear'));
+                    $('#liangwu').append(makeImg(104,-1.4,-1.5,'liangwu'));
+                    $('#lizi').append(makeImg(110,-1.4,-1.8,'lizi'));
+                    $('#grape').append(makeImg(160,-1.3,-1.8,'grape'));
+                break
+                case 7:
+                    $('#pumpkin').append(makeImg(74,-1.5,-1.6,'pumpkin'));
+                    $('#melon').append(makeImg(81.5,-2,-1.6,'melon'));
+                    $('#kongshintsai').append(makeImg(48,-1.6,-1.8,'kongshintsai'));
+                    $('#longshutsai').append(makeImg(84,-1.6,-1.6,'longshutsai'));
+                    $('#kukua').append(makeImg(101,-1.4,-1.5,'kukua'));
+                    $('#pear').append(makeImg(117.8,-1.8,-1.6,'pear'));
+                    $('#liangwu').append(makeImg(85,-1.4,-1.5,'liangwu'));
+                    $('#lizi').append(makeImg(97,-1.5,-1.8,'lizi'));
+                    $('#grape').append(makeImg(131,-1,-1.5,'grape'));
+                    $('#passionfruit').append(makeImg(90,-1.3,-1.8,'passionfruit'));
+                    $('#tauzi').append(makeImg(102,-1.2,-1.4,'tauzi'));
+                break
+                case 8:
+                    $('#broccoli').append(makeImg(82.7,-1.6,-1.5,'broccoli'));
+                    $('#uto').append(makeImg(221,-1.4,-1.4,'uto'));
+                    $('#pumpkin').append(makeImg(100,-1.5,-1.6,'pumpkin'));
+                    $('#melon').append(makeImg(66,-2,-1.6,'melon'));
+                    $('#kongshintsai').append(makeImg(65,-1.4,-1.8,'kongshintsai'));
+                    $('#kukua').append(makeImg(99,-1.8,-1.8,'kukua'));
+                    $('#tauzi').append(makeImg(120,-1.8,-2.2,'tauzi'));
+                    $('#pear').append(makeImg(120,-1.2,-1.2,'pear'));
+                    $('#lizi').append(makeImg(102,-1.7,-2,'lizi'));
+                    $('#grape').append(makeImg(90,-1.3,-1.6,'grape'));
+                    $('#passionfruit').append(makeImg(98,-1.3,-1.8,'passionfruit'));
+                    $('#liango').append(makeImg(380,-5,-3.5,'liango'));
+                break
+                case 9:
+                    $('#broccoli').append(makeImg(68,-2,-1.5,'broccoli'));
+                    $('#uto').append(makeImg(140,-1.6,-1.4,'uto'));
+                    $('#ponkan').append(makeImg(106,-1.6,-1.8,'ponkan'));
+                    $('#pumpkin').append(makeImg(87,-1.8,-1.6,'pumpkin'));
+                    $('#melon').append(makeImg(70.5,-2,-1.2,'melon'));
+                    $('#kongshintsai').append(makeImg(54,-1.4,-1.6,'kongshintsai'));
+                    $('#kukua').append(makeImg(115,-1.4,-1.6,'kukua'));
+                    $('#pear').append(makeImg(102,-1,-1.4,'pear'));
+                    $('#passionfruit').append(makeImg(97,-1.6,-1.6,'passionfruit'));
+                    $('#liango').append(makeImg(118,17.9,-1.6,'liango'));
+                    $('#papaya').append(makeImg(213,-1.1,-1.4,'papaya'));
+                break
+                case 10:
+                    $('#broccoli').append(makeImg(115,-1.6,-2,'broccoli'));
+                    $('#uto').append(makeImg(167,-1.6,-1.7,'uto'));
+                    $('#tonghau').append(makeImg(137,-1.6,-1.3,'tonghau'));
+                    $('#ponkan').append(makeImg(105,-2,-1.8,'ponkan'));
+                    $('#pumpkin').append(makeImg(81,-1.3,-1.4,'pumpkin'));
+                    $('#melon').append(makeImg(84.5,-2.2,-2,'melon'));
+                    $('#grape').append(makeImg(121,-5.6,-2,'grape'));
+                    $('#liango').append(makeImg(105,-1.3,-1.8,'liango'));
+                    $('#papaya').append(makeImg(205,-1.7,-1.9,'papaya'));
+                    $('#shizi').append(makeImg(91,-1.5,-1.5,'shizi'));
+                    $('#whitelobo').append(makeImg(97,4.3,-1.8,'whitelobo'));
+                break
+                case 11:
+                    $('#cabbage').append(makeImg(66,-1.7,-1.8,'cabbage'));
+                    $('#botsai').append(makeImg(76,-1.8,-1.6,'botsai'));
+                    $('#broccoli').append(makeImg(45,-1.4,-1.4,'broccoli'));
+                    $('#uto').append(makeImg(161,-1.4,-1.3,'uto'));
+                    $('#tonghau').append(makeImg(56,-1.8,-1.5,'tonghau'));
+                    $('#baitsai').append(makeImg(89,-1.5,-1.5,'baitsai'));
+                    $('#pea').append(makeImg(81,12.4,-1.2,'pea'));
+                    $('#orange').append(makeImg(102,-1.2,-1,'orange'));
+                    $('#ponkan').append(makeImg(111,-1.8,-1.8,'ponkan'));
+                    $('#melon').append(makeImg(69,-1.6,-1.6,'melon'));
+                    $('#grape').append(makeImg(127,-1.3,-1.4,'grape'));
+                    $('#shizi').append(makeImg(113,-1.8,-1.8,'shizi'));
+                    $('#whitelobo').append(makeImg(131,-1.8,-1.5,'whitelobo'));
+                break
+                // case 12:
+                //     $('#cabbage').append(makeImg(66,-1.7,-1.8,'cabbage'));
+                //     $('#botsai').append(makeImg(76,-1.8,-1.6,'botsai'));
+                //     $('#broccoli').append(makeImg(45,-1.4,-1.4,'broccoli'));
+                //     $('#uto').append(makeImg(161,-1.4,-1.3,'uto'));
+                //     $('#tonghau').append(makeImg(56,-1.8,-1.5,'tonghau'));
+                //     $('#baitsai').append(makeImg(89,-1.5,-1.5,'baitsai'));
+                //     $('#pea').append(makeImg(81,12.4,-1.2,'pea'));
+                //     $('#orange').append(makeImg(102,-1.2,-1,'orange'));
+                //     $('#strawberry').append(makeImg(79,-1.7,-1.5,'strawberry'));
+                //     $('#ponkan').append(makeImg(111,-1.8,-1.8,'ponkan'));
+                //     $('#grape').append(makeImg(127,-1.3,-1.4,'grape'));
+                //     $('#shizi').append(makeImg(113,-1.8,-1.8,'shizi'));
+                //     $('#whitelobo').append(makeImg(131,-1.8,-1.5,'whitelobo'));
+                // break
             }
             
 
